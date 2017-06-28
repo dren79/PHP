1d81b5e5ee6c8844dd71742f651e6e7d150b4366
dren79 <davidleerenton@gmail.com>
2017-06-28 19:46:58 +0100

    PHP and HTML

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-$myVar = "Hello Dave";
-echo "<b>". $myVar."</b>";
+$myVar = "Hello world!";
 ?>
+<i>We have HTML <u>all</u> over the place here!</i><br />
+<i>But some PHP as well, as you can see:</i><br />
+A message from PHP: <b><?php echo $myVar; ?></b>
