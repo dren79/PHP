61c218778bc1fa42da63965cb1f5f052df18d76a
dren79 <davidleerenton@gmail.com>
2017-06-28 20:34:14 +0100

    Data types in PHP

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-$myVar = 42;
-if($myVar = 42)
-{
-    ?>
-    <b>Yes. This number is 42!</b>
-    <?php
-}
+$number = 42;
+$number2 = "32";
+if($number === 42){echo "The number is 42";}
+?>
+  </br>
+<?php  
+if($number2 === 32){echo "The number is 32";}//won't print 32 is a string not an int
+    
 ?>
