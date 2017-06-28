0a3281c19a52048675c73e9ee5ac2f68b54fa8d8
dren79 <davidleerenton@gmail.com>
2017-06-28 19:59:20 +0100

    If statement PHP

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$myVar = "Hello world!";
+$myVar = 42;
+if($myVar = 42)
+{
+    ?>
+    <b>Yes. This number is 42!</b>
+    <?php
+}
 ?>
-<i>We have HTML <u>all</u> over the place here!</i><br />
-<i>But some PHP as well, as you can see:</i><br />
-A message from PHP: <b><?php echo $myVar; ?></b>
