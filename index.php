6cf1627823f4a5a13ad08d1d6a2c3c6e4a993f0a
dren79 <davidleerenton@gmail.com>
2017-06-29 08:30:24 +0100

    ternary operator is like a short version of an if-and-else statement

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-$number = 42;
-$number2 = "32";
-if($number === 42){echo "The number is 42";}
-?>
-  </br>
-<?php  
-if($number2 === 32){echo "The number is 32";}//won't print 32 is a string not an int
-    
+$numberOfItems = 0;
+$output = "You have " . (($numberOfItems > 0) ? "something" : "nothing") . " in your bag";
+echo $output;
 ?>
